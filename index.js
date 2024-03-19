a6f35f43457be4e80ecc5049b128d886efaef52f
hyunji <phj9573@naver.com>
2024-03-19 19:39:34 +0900

    fix

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function (req, res) {
     res.send('초기 화면 입니당')
 })
 
+app.get('/apple', function (req, res) {
+    res.send('apple 입니다')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
